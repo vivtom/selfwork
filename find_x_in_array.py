a4cd0067808cbe7c3c7934233e37aacf1b0c75ec
vivtom <vtomar155@gmail.com>
2025-01-03 15:38:55 +0530

    Create find_x_in_array.py

---
+++ find_x_in_array.py
@@ -0,0 +1,11 @@
+array = input('Enter array : ').split()
+x = input('what do you want to find? : ')
+found = False
+for i in range(len(array)): #we want to stop after the last element hence here we wanted to use the indexes hence we used in range(len(array))
+        if x == array[i]:
+            print(i)
+            found = True
+            break
+if not found :
+        print('it does not exist ')
+        
