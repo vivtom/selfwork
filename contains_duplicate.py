30232ea16323cf852aaeb5fd89a1fa523e6722c5
vivtom <vtomar155@gmail.com>
2025-05-13 17:26:36 +0530

    blind 75

---
+++ contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        set1 = set()
+        for i in nums:
+            if i in set1:
+                return True
+            else:
+                set1.add(i)
+        return False
