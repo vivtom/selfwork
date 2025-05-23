a46c0cfa0764249f3fbe1ed1b4decffefdc8429a
vivtom <vtomar155@gmail.com>
2025-05-23 17:41:22 +0530

    blind 1

---  contains_Duplicate.py
+++  contains_Duplicate.py
@@ -1,9 +1,9 @@
-set1 = set()
-def contains_duplicate(arr):
-    for i in arr:
-        if i in set1:
-            return True
-        else:
-            set1.add(i)
-    return False
-print(contains_duplicate([1,2,3]))
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        set1 = set()
+        for i in nums:
+            if i in set1:
+                return True
+            else:
+                set1.add(i)
+        return False
