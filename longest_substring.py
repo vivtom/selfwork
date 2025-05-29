207f5ccd4a19691218d7887837d99c7243f2e943
vivtom <vtomar155@gmail.com>
2025-05-29 19:51:59 +0530

    blind 16

--- longest_substring.py
+++ longest_substring.py
@@ -1,13 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
-        l = 0
-        res = 0
-
+        set1 = set()
+        l = 0 
+        max_count = 0
         for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res
+            while s[r] in set1:
+                set1.remove(s[l])
+                l +=1 
+            set1.add(s[r])
+            max_count = max(max_count,(r-l)+1)
+        return max_count
