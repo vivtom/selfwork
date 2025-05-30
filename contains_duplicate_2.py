4a05bbf96f3f7c91e9336ec9de3292a78a84a6bb
vivtom <vtomar155@gmail.com>
2025-05-30 19:54:44 +0530

    sliding window

--- contains_duplicate_2.py
+++ contains_duplicate_2.py
@@ -1,4 +1,19 @@
-#   O(n^2) approach using 2 pointers (not very efficient)
+# O(n) approach
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        set1 = set()
+        l = 0
+        for r in range(len(nums)):
+            if (r-l) > k:
+                set1.remove(nums[l])
+                l +=1 
+            if nums[r] in set1:
+                return True
+            set1.add(nums[r])
+        return False
+
+
+#   O(n^2) approach using 2 pointers (not efficient, ignore)
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         l = 0 
@@ -16,3 +31,4 @@ class Solution:
             else:
                 r += 1
         return False
+    
