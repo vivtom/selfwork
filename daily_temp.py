e62e2f90d1099fc213f536884f553a4405c1e17f
vivtom <vtomar155@gmail.com>
2025-06-05 17:47:56 +0530

    blind 25

---
+++ daily_temp.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        output = [0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                output[stackInd] = (i - stackInd)
+            stack.append([t,i])
+        return output
+
+
+        
